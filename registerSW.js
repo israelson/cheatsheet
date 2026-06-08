7e0c844491fc855356dcd4ae107194f9e18984f5
Israelson Dias <israelson.dias@gmail.com>
2026-06-08 19:59:00 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cheatsheet/sw.js', { scope: '/cheatsheet/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
